9f7e7482e23065a8230244b826338c7b5a1c4005
“Lucas” <lucas.baudson2@etu.univ-lorraine.fr>
2019-01-09 12:05:48 +0100

    wireframe correct

--- base.cpp
+++ base.cpp
@@ -13,24 +13,34 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 vector<vector<string>> vlignes;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-
-    if(abs(x0 - x1) >= abs(y0 - y1)){
-       swap(x0, y0);
-       swap(x1, y1);
+void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool swaped = false;
+    if (abs(x0 - x1) < abs(y0 - y1)) {
+        swap(x0, y0);
+        swap(x1, y1);
+        swaped = true;
     }
-
-    if(x0 > x1){
+    if (x0 > x1) {
         swap(x0, x1);
         swap(y0, y1);
     }
-
-    for (int x=x0; x<=x1; x++) {
-        float t = (x-x0)/(float)(x1-x0);
+    for (int x = x0; x <= x1; x++) {
+        float t = (x - x0)/(float)(x1 - x0);
         int y = y0*(1.-t) + y1*t;
-        image.set(x, y, color);
+        if (swaped) {
+            image.set(y, x, color);
+        } else {
+            image.set(x, y, color);
+        }
     }
 }
+
+
+
+
+
+
+
 
 
 
@@ -119,11 +129,11 @@ int drawLineTga(string name, TGAImage &image){
                 vector<string> point3 = vlignes[-1+stoi(vstrings[3].substr(0, vstrings[3].find("/")))];
 
 
-                line((1.+(float)strtof((point1[1]).c_str(),0))*400, (1.+(float)strtof((point1[2]).c_str(),0))*400, (1.+(float)strtof((point2[1]).c_str(),0))*400, (1.+(float)strtof((point2[2]).c_str(),0))*400, image, white);
+                drawLine((1.+(float)strtof((point1[1]).c_str(),0))*400, (1.+(float)strtof((point1[2]).c_str(),0))*400, (1.+(float)strtof((point2[1]).c_str(),0))*400, (1.+(float)strtof((point2[2]).c_str(),0))*400, image, white);
 
-                line((1.+(float)strtof((point2[1]).c_str(),0))*400, (1.+(float)strtof((point2[2]).c_str(),0))*400, (1.+(float)strtof((point3[1]).c_str(),0))*400, (1.+(float)strtof((point3[2]).c_str(),0))*400, image, white);
+                drawLine((1.+(float)strtof((point2[1]).c_str(),0))*400, (1.+(float)strtof((point2[2]).c_str(),0))*400, (1.+(float)strtof((point3[1]).c_str(),0))*400, (1.+(float)strtof((point3[2]).c_str(),0))*400, image, white);
 
-                line((1.+(float)strtof((point3[1]).c_str(),0))*400, (1.+(float)strtof((point3[2]).c_str(),0))*400, (1.+(float)strtof((point1[1]).c_str(),0))*400, (1.+(float)strtof((point1[2]).c_str(),0))*400, image, white);
+                drawLine((1.+(float)strtof((point3[1]).c_str(),0))*400, (1.+(float)strtof((point3[2]).c_str(),0))*400, (1.+(float)strtof((point1[1]).c_str(),0))*400, (1.+(float)strtof((point1[2]).c_str(),0))*400, image, white);
 
                 //float x =  strtof((vstrings[1]).c_str(),0);
                 //float y =  strtof((vstrings[2]).c_str(),0);
@@ -147,7 +157,7 @@ int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
     image.set(52, 41, red);
 
-    line(10, 20, 80, 80, image, white);
+    drawLine(10, 20, 80, 80, image, white);
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
